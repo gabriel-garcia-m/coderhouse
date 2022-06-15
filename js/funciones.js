58bae50b116d0c1f39e7bed48dc0f2aaeac55b3f
Gabriel Garcia <42195mx@pm.me>
2022-06-14 22:01:38 -0500

    Primera entrega proyecto final Javascript

--- js/funciones.js
+++ js/funciones.js
@@ -1,7 +1,8 @@
+//Función principal que controla flujo
 function ejecuta() {
   let seleccionOp = parseInt(
     prompt(
-      "Seleccionar opciones: \n Ingresa 1: Calcula promedio para los siguientes cinco años (CDMX, México) \n Ingresa 2: Consulta histórico de últimos diez años (CDMX, México) \n Ingresa 3: Salir"
+      "Seleccionar opciones: \n Ingresa 1: Calcula promedio para los siguientes cinco años (CDMX, México) \n Ingresa 2: Consulta histórico de últimos diez años (CDMX, México) \n Ingresa 3: Convierte grados Celcius a Fahrenheit \n Ingresa 4: Convierte grados Fahrenheit a Celcius \n Ingresa 5 para salir"
     )
   );
 
@@ -21,17 +22,35 @@ function ejecuta() {
       break;
 
     case 3:
+      celciusFahrenheit(
+        parseFloat(
+          prompt("Convierte grados Celcius a Fahrenheit - ingresa valor:")
+        )
+      );
+      break;
+
+    case 4:
+      fahrenheitCelcius(
+        parseFloat(
+          prompt("Convierte grados Fahrenheit a Celcius - ingresa valor:")
+        )
+      );
+
+      break;
+
+    case 5:
       break;
 
     default:
       alert(
-        "Valor incorrecto, solo puedes ingresar 1 o 2. Intenta de nuevo..."
+        "Valor incorrecto, solo puedes ingresar 1, 2, 3, 4 o 5. Intenta de nuevo..."
       );
       ejecuta();
       break;
   }
 }
 
+//Función para calcular promedio
 function promedioCDMX() {
   let anio1 = parseFloat(prompt("Ingresa valor para año 2023"));
   let anio2 = parseFloat(prompt("Ingresa valor para año 2024"));
@@ -43,8 +62,10 @@ function promedioCDMX() {
       (anio1 + anio2 + anio3 + anio4 + anio5) / 5 +
       " grados centígrados."
   );
+  ejecuta()
 }
 
+//Función para obtener temperatura histórica, utilizando funciones de orden superior y arreglo de objetos
 function obtieneTemp(anio) {
   const resultado = aniosArreglo.find((tem) => tem.anio === anio);
   if (resultado == undefined) {
@@ -53,7 +74,8 @@ function obtieneTemp(anio) {
     );
     ejecuta();
   } else {
-    alert(resultado.temperatura);
+    alert("La temperatura en el año " + anio+ " fue de " + resultado.temperatura + " grados Celcius");
+    ejecuta()
   }
 }
 
@@ -147,3 +169,35 @@ function obtieneTemp(anio) {
 //       break;
 //   }
 // }
+
+//Función para convertir grados Celcius a Fahrenheit
+function celciusFahrenheit(temperatura) {
+  if (isNaN(temperatura)) {
+    alert("Ingresa un número...");
+    ejecuta();
+  } else {
+    alert(
+      temperatura +
+        " grados Celcius a Fahrenheit son: " +
+        (temperatura * 1.8 + 32).toFixed(2) +
+        " grados"
+    );
+    ejecuta();
+  }
+}
+
+//Función para convertir grados Fahrenheit a Celcius
+function fahrenheitCelcius(temperatura) {
+  if (isNaN(temperatura)) {
+    alert("Ingresa un número...");
+    ejecuta();
+  } else {
+    alert(
+      temperatura +
+        " grados Fahrenheit a Celcius son: " +
+        ((temperatura - 32) / 1.8).toFixed(2) +
+        " grados"
+    );
+    ejecuta();
+  }
+}
